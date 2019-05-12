3bbe3a59c724f5147074e93a93853f998fbc269f
MichaelGWorkman <michael.g.workman@gmail.com>
2019-05-12 12:11:52 -0400

    Updated Bootstrap CSS Styles and also added alternating color rows for location distances table.

--- GeoSpatial/DAL/GeoSpatialContext.cs
+++ GeoSpatial/DAL/GeoSpatialContext.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using GeoSpatial.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
